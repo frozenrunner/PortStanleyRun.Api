ddaf5281650d4bdae9acd42c0c44be548154c45a
frozenrunner <mike.horlick@gmail.com>
2023-03-29 16:49:26 -0400

    oh yeah, this file too

---
+++ PortStanleyRun.Api/Models/RunParticipant.cs
@@ -0,0 +1,10 @@
+﻿using MongoDB.Bson;
+
+namespace PortStanleyRun.Api.Models
+{
+    public class RunParticipant
+    {
+        public ObjectId RunnerId { get; set; }
+        public string StartingPoint { get; set; } = string.Empty;
+    }
+}
