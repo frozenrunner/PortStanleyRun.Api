d32093154452da85c6fa7cd3d5ca0aa628e21856
frozenrunner <mike.horlick@gmail.com>
2023-03-30 14:02:25 -0400

    Comments and a pull request template

---
+++ .github/pull_request_template.md
@@ -0,0 +1,9 @@
+### What does this change accomplish?
+
+### What was your approach and why did you choose it?
+
+### What, if anything, should reviewers look out for when reviewing this change?
+
+### Related documentation
+
+### Commits
--- PortStanleyRun.Api/Models/RunParticipant.cs
+++ PortStanleyRun.Api/Models/RunParticipant.cs
@@ -4,7 +4,14 @@ namespace PortStanleyRun.Api.Models
 {
     public class RunParticipant
     {
+        /// <summary>
+        /// ObjectId of a PortStanleyUser
+        /// </summary>
         public ObjectId RunnerId { get; set; }
+
+        /// <summary>
+        /// Starting point for the run this shows up in
+        /// </summary>
         public string StartingPoint { get; set; } = string.Empty;
     }
 }
